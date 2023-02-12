5eb5405898eda04c4a8d3166ee2e32904a64b49d
Angel Abse Hidalgo <angel.abse@tinet.cl>
2023-02-12 00:10:31 +0000

    Added disintegration of option discarded

--- js/main.js
+++ js/main.js
@@ -1,4 +1,29 @@
+disintegrate.init();
+
+// Made a vote
 function vote(event) {
-  console.log(event);
-  event.target.parentElement.classList.add('option--voted');
+  const parent = event.target.parentElement;
+  parent.classList.add('option--voted');
+  removeDiscardVote(parent.getAttribute("data-option-discard"));
+}
+
+// Remove option discarded
+function removeDiscardVote(optionNumber) {
+  let classElem = `.option-${optionNumber}`;
+
+  // get elements to remove
+  const elem = document.querySelector(classElem);
+  const elemVs = document.querySelector('.vs');
+
+  // create disintegrate elements
+  const disObj = disintegrate.getDisObj(elem);
+  const disObjVs = disintegrate.getDisObj(elemVs);
+
+  // create disintegrate animation
+  disintegrate.createSimultaneousParticles(disObj);
+  disintegrate.createSimultaneousParticles(disObjVs);
+
+  // remove elements
+  elem.remove();
+  elemVs.remove();
 }
