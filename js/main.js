5f18048afc77cffc42149962234dbde35ef641ac
Angel Abse Hidalgo <angel.abse@tinet.cl>
2023-02-16 00:05:14 +0000

    Added creation of new vote

--- js/main.js
+++ js/main.js
@@ -14,19 +14,21 @@ function removeDiscardVote(optionNumber) {
 
   // get elements to remove
   const elem = document.querySelector(classElem);
-  const elemVs = document.querySelector('.vs');
+  //const elemVs = document.querySelector('.vs');
+
+  //console.log(elemVs);
 
   // create disintegrate elements
   const disObj = disintegrate.getDisObj(elem);
-  const disObjVs = disintegrate.getDisObj(elemVs);
+  //const disObjVs = disintegrate.getDisObj(elemVs);
 
   // create disintegrate animation
   disintegrate.createSimultaneousParticles(disObj);
-  disintegrate.createSimultaneousParticles(disObjVs);
+  //disintegrate.createSimultaneousParticles(disObjVs);
 
   // remove elements
   elem.remove();
-  elemVs.remove();
+  //elemVs.remove();
 }
 
 function addButtonNewVote() {
@@ -42,4 +44,50 @@ function addButtonNewVote() {
 function newVote() {
   const option = document.querySelector(".option--voted");
   option.classList.add("option--return");
+
+  setTimeout(() => {
+    option.className = "option option-1";
+    option.setAttribute("data-option-discard", "2");
+
+    const disObj = disintegrate.getDisObj(option);
+    disintegrate.createSimultaneousParticles(disObj);
+    createVote();
+  }, 1700);
+}
+
+function createVote() {
+  const voteElement = document.createElement("div");
+  voteElement.setAttribute("data-dis-type", "simultaneous");
+  voteElement.setAttribute("data-option-discard", "1");
+  voteElement.classList.add("option");
+  voteElement.classList.add("option-2");
+
+  const voteContent = document.createElement("div");
+  voteContent.classList.add("content-vote");
+
+  const voteButton = document.createElement("div");
+  voteButton.classList.add("vote");
+  voteButton.innerHTML = "Votar";
+
+  voteElement.appendChild(voteContent);
+  voteElement.appendChild(voteButton);
+
+  //createVs();
+
+  const container = document.querySelector(".container");
+  container.appendChild(voteElement);
+  
+  voteButton.addEventListener("click", (event) => {
+    vote(event);
+  });
+}
+
+function createVs() {
+  const vsElement = document.createElement("div");
+  vsElement.setAttribute("data-dis-type", "simultaneous");
+  vsElement.classList.add("vs");
+  vsElement.innerHTML = "VS";
+
+  const container = document.querySelector(".container");
+  container.appendChild(vsElement);
 }
